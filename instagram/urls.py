c869cc4c0383a4b960592a1ef1d0002e2c200565
kadas36 <oduorthomas14@gmail.com>
2021-01-17 23:58:22 +0300

    Add new post template

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.insta_home, name='home')
+    path('', views.insta_home, name='home'),
+    path('post/', views.new_post, name='newpost'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from instagram.models import Profile
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import commentForm, postForm
+
+
+
 
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
 def insta_home(request):
-    return render(request, 'all_insta/home.html')
+    current_user = request.user
+    if request.method == 'POST':
+        form = commentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.editor = current_user
+            comment.save()
+        return redirect('home')
+
+    else:
+        form = commentForm()
+    return render(request, 'all_insta/home.html', {"form": form})
+
+
+def new_post(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user = current_user.id)
+    
+    if request.method == 'POST':
+        form = postForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.profile_key = profile.id
+            image.user = current_user.id
+            image.save()
+        return redirect('home') 
+
+    else:
+        form = postForm()
+
+    return render(request, 'all_insta/new_post.html', {"new_post": form})
