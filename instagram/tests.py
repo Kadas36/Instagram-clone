5d24d49dd8865b005570aead8d9d2244f9aa12d9
kadas36 <oduorthomas14@gmail.com>
2021-01-17 23:57:52 +0300

    Add tests

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.forms import UsernameField
 from django.test import TestCase
+from .models import Image,Comment,Friend,Profile,User
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        User.james= Profile(user = 'james')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Profile))  
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.james.save_editor()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)       
