d411858f140dd8af6e45ddb8e50eff700ed907af
kadas36 <oduorthomas14@gmail.com>
2021-01-18 06:55:18 +0300

    Update comment form

--- instagram/forms.py
+++ instagram/forms.py
@@ -6,19 +6,19 @@ from .models import Comment, Image
 class commentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        exclude = ['editor', 'pub_date']
+        exclude = ['editor', 'pub_date', 'image']
         widgets = {
             'comment': forms.Textarea(
-                attrs={'placeholder': 'add a comment', 'cols': 1000, 'rows': 1}),
+                attrs={'placeholder': 'add a comment', 'rows': 1}),
                 
         }
 
 class postForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['user', 'pub_date', 'comments', 'likes', 'profile_key']
+        exclude = ['user', 'pub_date', 'comments', 'likes', 'profile_key', 'user_key']
         widgets = {
             'caption': forms.Textarea(
-                attrs={'placeholder': 'add a caption', 'cols': 100, 'rows': 1}),
+                attrs={'placeholder': 'add a caption', 'rows': 2}),
                 
         }        
